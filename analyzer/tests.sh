60dc6f7bec506baeede6f91cf796914c0c551f4c
Thibault <t.dardinier@gmail.com>
2018-12-03 23:44:44 +0100

    More tests

--- analyzer/tests.sh
+++ analyzer/tests.sh
@@ -1,3 +1,7 @@
 python3 analyzer.py ../mnist_nets/mnist_relu_3_10.txt ../mnist_images/img0.txt 0.0344
 python3 analyzer.py ../mnist_nets/mnist_relu_3_10.txt ../mnist_images/img1.txt 0.0397
 python3 analyzer.py ../mnist_nets/mnist_relu_3_20.txt ../mnist_images/img0.txt 0.025
+python3 analyzer.py ../mnist_nets/mnist_relu_3_50.txt ../mnist_images/img0.txt 0.01
+python3 analyzer.py ../mnist_nets/mnist_relu_3_50.txt ../mnist_images/img1.txt 0.01
+python3 analyzer.py ../mnist_nets/mnist_relu_6_100.txt ../mnist_images/img21.txt 0.001
+python3 analyzer.py ../mnist_nets/mnist_relu_6_20.txt ../mnist_images/img24.txt 0.005
