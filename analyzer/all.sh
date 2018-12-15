22e70ba51868ec37ba83ac504538f9024ad1d7de
Thibault <t.dardinier@gmail.com>
2018-12-15 17:22:51 +0100

    4_1024

--- analyzer/all.sh
+++ analyzer/all.sh
@@ -2,6 +2,6 @@ for image in ../mnist_images/*; do
     echo "verif: $image"
     for net in ../mnist_nets/*; do
         echo "verif: $net"
-        eval "python3 analyzer.py ../mnist_nets/$net ../mnist_images/$image 0.005"
+        python3 analyzer.py ../mnist_nets/$net ../mnist_images/$image 0.005
     done
 done
